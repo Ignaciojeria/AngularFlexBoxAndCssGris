65789743c5a9b24ec118b7e10aaa14496896c3d9
ignacio <Ignaciovl.j@gmail.com>
2017-09-17 03:08:06 -0700

    Agregando el numero 2 a un lugar ramdom del grid

--- src/app/cell/cell.component.ts
+++ src/app/cell/cell.component.ts
@@ -10,13 +10,50 @@ export type fieldState = number[][];
   styleUrls: ['./cell.component.css']
 })
 export class CellComponent implements OnInit {
+  
+  private size:number=4;
 
-  private state= new BehaviorSubject<fieldState>(new Array(4).fill(new Array(4).fill(new Array(null))));
+  private state= new BehaviorSubject<fieldState>(new Array(this.size).fill(null).map(_=>new Array(this.size).fill(null)));
+
+  private add2inRamdomCell():void{
+    
+    let field = this.state.value;
+
+    let one=Math.floor(Math.random()* this.size);
+    let two=Math.floor(Math.random()* this.size);
+  for (var i = 0; i < field.length; i++) {
+    for (var z = 0; z < field.length; z++) {
+      if(this.state.value[i][z]===null){
+        if(this.state.value[one][two]==2){
+          this.state.value[i][z]=2;
+          return;
+        }
+        this.state.value[one][two]=2;
+        console.log("paso");
+        return;
+      }
+    } 
+  }
+    /* 
+    field.forEach((row,rowIndex) => {
+      row.forEach((tile,tileIndex) =>{
+        if(this.state.value[rowIndex][tileIndex]===null){
+          this.state.value[one][two]=2;
+          console.log("paso");
+        }
+      });
+    }
+  );*/
+  }
+
+
 
   constructor() { }
 
+
   ngOnInit() {
     console.log(this.state.value);
+
   }
 
 }
